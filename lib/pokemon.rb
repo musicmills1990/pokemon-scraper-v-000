3f19eebfdace1025c32c1c460ed7471063032358
Marc Mills <mdmills1990@gmail.com>
2018-10-25 15:32:35 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,8 +12,9 @@ attr_accessor :id, :name, :type, :db
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
   end
-end
 
 def self.find
+
+end
 
 end
