fb4cbfb623778bf9b41234d3dccd0aa59ed72f44
Marc Mills <mdmills1990@gmail.com>
2018-10-25 15:47:49 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,9 +14,13 @@ attr_accessor :id, :name, :type, :db, :hp
     db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
   end
 
-def self.find(id_num, db)
-  info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
-  Pokemon.new(id: info[0], name: info[1], type: info[2], hp: info[3], db: db)
-end
+  def self.find(id_num, db)
+    info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: info[0], name: info[1], type: info[2], hp: info[3], db: db)
+  end
+
+  def alter_hp(new_hp, db)
+    db.execute("UPDATE pokemon SET = ? WHERE id = ?", new_hp, self.id)
+  end
 
 end
